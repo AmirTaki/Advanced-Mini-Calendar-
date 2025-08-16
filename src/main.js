5d9eb0875c81a62b9af792ce56259370b1bedc32
amir <amirtaki623@gmail.com>
2025-08-16 21:41:01 +0330

    part 16

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 const date = new Date();
 const dayNum = date.getDay()
-
-
 const active = document.querySelector(".week li:nth-child("+dayNum+")")
 
 
+const day = date.getDate();
+
 active.classList.add('current')
+
+const h1 = document.createElement('h1');
