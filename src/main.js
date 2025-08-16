8b89e4e5dc08562e3a6517ed1014e036b23450d8
amir <amirtaki623@gmail.com>
2025-08-16 21:53:46 +0330

    part 22

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const active = document.querySelector(".week li:nth-child("+dayNum+")")
 
 const day = date.getDate();
 const month = months[date.getMonth()]
-
+const year = date.getFullYear()
 
 active.classList.add('current')
 
@@ -20,4 +20,10 @@ active.appendChild(h1)
 const h5 = document.createElement("h5")
 h5.textContent = month;
 active.appendChild(h5)
+
+
+//year 
+const h3  = document.createElement("h3")
+h3.textContent = year;
+active.appendChild(h3)
 
