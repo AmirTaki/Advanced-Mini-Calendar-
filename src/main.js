07c0eb105ed0d9f89fd38d83266faa4402c08e97
amir <amirtaki623@gmail.com>
2025-08-16 21:49:59 +0330

    part 20

--- src/main.js
+++ src/main.js
@@ -6,9 +6,18 @@ const active = document.querySelector(".week li:nth-child("+dayNum+")")
 
 
 const day = date.getDate();
-const month = date.getMonth();
+const month = months[date.getMonth()]
+
+
 active.classList.add('current')
 
+// day
 const h1 = document.createElement('h1');
 h1.textContent = day;
 active.appendChild(h1)
+
+// month
+const h3 = document.createElement("h3")
+h3.textContent = month;
+active.appendChild(h3)
+
